50be88f455b8a2b1ae927c40d9ec75552f028925
SF-codes <felicissimo.shanis@gmail.com>
2021-12-31 14:00:02 -0500

    Improved Stripe implemtation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -51,6 +52,14 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in 1 hour!',
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({
+    type: 'accplication/json',
+  }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(
