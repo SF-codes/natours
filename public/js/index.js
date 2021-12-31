6fbd1c4cf597fbc163af73e8a25668b7c94bb79e
SF-codes <felicissimo.shanis@gmail.com>
2021-12-31 16:20:02 -0500

    more signup additions

--- public/js/index.js
+++ public/js/index.js
@@ -63,6 +63,16 @@ if (userPasswordForm)
     document.getElementById('password-confirm').value = '';
   });
 
+if (bookBtn)
+  bookBtn.addEventListener('click', (e) => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
+
 if (signupForm) {
   signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -73,13 +83,3 @@ if (signupForm) {
     signup(name, email, password, confirmPassword);
   });
 }
-
-if (bookBtn)
-  bookBtn.addEventListener('click', (e) => {
-    e.target.textContent = 'Processing...';
-    const { tourId } = e.target.dataset;
-    bookTour(tourId);
-  });
-
-const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 20);
