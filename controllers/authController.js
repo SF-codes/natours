e09045ce9fc86231adb244cdfbc6502309b21dd3
SF-codes <felicissimo.shanis@gmail.com>
2021-12-31 21:13:53 -0500

    env bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -231,7 +231,6 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   // 3) Update changedPasswordAt property for the users
-
   // 4) Log the user in, send JWT
   createSendToken(user, 200, req, res);
 });
